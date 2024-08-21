99fec1964a42f116a097683da04b3a43d78beeb6
Chinenye <badnenye@gmail.com>
2024-08-21 19:59:42 +0100

    added the human choice

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function getComputerChoice(hands) {
   return hands[randomIndex];
 }
 console.log(getComputerChoice(hands));
+
+function getHumanChoice() {
+  let myChoice = prompt("Choose between rock, paper, or scissors");
+
+  if (myChoice === "rock") {
+    return "Rock";
+  } else if (myChoice === "paper") {
+    return "Paper";
+  } else if (myChoice === "scissors") {
+    return "Scissors";
+  } else {
+    alert("Reload and choose from the option given");
+  }
+}
+console.log(getHumanChoice());
