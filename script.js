3deb6e327998e2c5dbb3ecd22c8cf017397edeb8
Chinenye <badnenye@gmail.com>
2024-08-24 09:15:23 +0100

    Nothing new

--- script.js
+++ script.js
@@ -26,3 +26,44 @@ function getHumanChoice() {
     return null;
   }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playround(humanChoice, computerChoice) {
+  if (humanChoice === null) {
+    alert("Game canceled.");
+    return;
+  }
+
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    alert("You win, Rock beats Scissors.");
+    humanScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    alert("You lose, Rock beats Scissors.");
+    computerScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    alert("You lose, Scissors beats Paper.");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    alert("You win, Scissors beats Paper.");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    alert("You win, Paper beats Rock.");
+    humanScore++;
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    alert("You lose, Paper beats Rock.");
+    computerScore++;
+  } else if (humanChoice === computerChoice) {
+    alert("It's a draw.");
+  }
+
+  console.log(
+    `Human choice: ${humanChoice}, Computer choice: ${computerChoice}`
+  );
+  console.log(`Score -> Human: ${humanScore}, Computer: ${computerScore}`);
+}
+playround(getHumanChoice(), getComputerChoice());
